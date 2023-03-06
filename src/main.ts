d31d70db53d3b502d9af274bbf9d6cb1f98a3a35
yingjieweb <1184061078@qq.com>
2023-03-06 23:22:25 +0800

    feat: 🎸 add basic request feature

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,39 @@
-console.log('Hello world!');
+import * as https from 'https'
+import * as querystring from 'querystring'
+import md5 from 'md5';
+
+export const translate = (word: string) => {
+  console.log('current word', word);
+
+  const appId = ''
+  const appSecret = ''
+  const salt = Math.random()
+  const sign = md5(appId + word + salt + appSecret)
+
+  const query: string = querystring.stringify({
+    q: word,
+    from: 'en',
+    to: 'zh',
+    appid: appId,
+    salt: Math.random(),
+    sign: sign
+  })
+
+  const options = {
+    hostname: 'api.fanyi.baidu.com',
+    port: 443,
+    path: '/api/trans/vip/translate',
+    method: 'GET'
+  }
+
+  const req = https.request(options, (res) => {
+    res.on('data', (d) => {
+      process.stdout.write(d)
+    })
+  })
+
+  req.on('error', (e) => {
+    console.error(e);
+  })
+  req.end()
+}
