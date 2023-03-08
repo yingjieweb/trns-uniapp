0f9757aff31ab12c3f3450c7c47998f7b2add07a
yingjieweb <1184061078@qq.com>
2023-03-08 21:32:26 +0800

    feat: 🎸 implement translate english to chinese

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import * as https from 'https'
 import * as querystring from 'querystring'
 import md5 from 'md5';
+import { appId, appSecret } from './private';
+
+type BaiduTranslateRes = {
+  error_code?: string;
+  error_msg?: string;
+  form: string;
+  to: string;
+  trans_result: {
+    src: string;
+    dst: string;
+  }[]
+}
 
 export const translate = (word: string) => {
-  console.log('current word', word);
-
-  const appId = ''
-  const appSecret = ''
   const salt = Math.random()
   const sign = md5(appId + word + salt + appSecret)
 
@@ -15,25 +23,37 @@ export const translate = (word: string) => {
     from: 'en',
     to: 'zh',
     appid: appId,
-    salt: Math.random(),
+    salt: salt,
     sign: sign
   })
 
   const options = {
     hostname: 'api.fanyi.baidu.com',
     port: 443,
-    path: '/api/trans/vip/translate',
+    path: '/api/trans/vip/translate?' + query,
     method: 'GET'
   }
 
-  const req = https.request(options, (res) => {
-    res.on('data', (d) => {
-      process.stdout.write(d)
+  const request = https.request(options, (response) => {
+    const chunks: any = []
+    response.on('data', (chunk) => {
+      chunks.push(chunk);
+    })
+    response.on('end', () => {
+      const string = Buffer.concat(chunks).toString()
+      const transRes: BaiduTranslateRes = JSON.parse(string)
+      if (transRes.error_code) {
+        console.log(transRes.error_msg);
+        process.exit(2)
+      } else {
+        console.log(transRes.trans_result[0].dst);
+        process.exit(0)
+      }
     })
   })
 
-  req.on('error', (e) => {
+  request.on('error', (e) => {
     console.error(e);
   })
-  req.end()
+  request.end()
 }
